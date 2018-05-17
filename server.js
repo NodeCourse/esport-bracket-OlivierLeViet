8723572d6894f8d1f932d8ea104b2e6e446b8eae
OlivierLeViet <orsn.tran@free.fr>
2018-05-17 10:28:57 +0200

    un petit peu mieux
    zerze

--- server.js
+++ server.js
@@ -7,21 +7,13 @@ const LocalStrategy = require('passport-local');
 const COOKIE_SECRET = 'cookie secret';
 const Sequelize = require('sequelize');
 
-
-const KIMY = {
-    id: 1,
-    firstname: 'kim',
-    lastname: 'jong_un',
-    email: 'pyongyang@redstar.com',
-    password: 'missile_nuclaire'
-};
-
 //DB
 // connexion à la base de données, new Sequelize( nom de la db, id/pseudo, password)
 const db = new Sequelize('blog', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 });
+
 //Définition de la table articles dans le js
 const Users = db.define('users', {
     pseudo: {
@@ -40,28 +32,29 @@ const Users = db.define('users', {
 passport.use(new LocalStrategy((email, pwd, cb) => {
 
     Users
-        .findOne({email: email, pwd: pwd})
+        .findOne({where: {email: email, pwd: pwd}})
         .then((user) => {
             cb(null, user || false);
         });
-    
+
 }));
 passport.serializeUser((user, cb) => {
     cb(null, user.email);
 });
 passport.deserializeUser((email, cb) => {
-
-
     Users
-        .findOne({email: email})
+        .findOne({where: {email: email}})
         .then((user) => {
             return cb(null, user || false);
-        });
+        })
+        .catch(cb);
+
 });
 
 // middleware à ne pas changer
 const app = express();
 app.set('view engine', 'pug');
+app.use(express.static("public"));
 app.use(cookieParser(COOKIE_SECRET));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({secret: COOKIE_SECRET, resave: false, saveUninitialized: false}));
@@ -69,37 +62,39 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-//route de base, sur index.pug,
-app.get('/', (req, res) => {
-    Users
-        .sync() // création de la table
-        .then(() => {
-            Users
-                .findAll()
-                .then((users) => {
-                    res.render('home', {users}); //récupère et envoie les données
-                })
-        });
-});
-
 //lorsque l'on submit
 app.post('/api/post', (req, res) => {
     const pseudo = req.body.pseudo;
     const pwd = req.body.pwd;
     const email = req.body.email;
-    Users    //nouveaux articles cré dans la base
+    Users    //nouveaux articles créé dans la base
         .create({pseudo: pseudo, pwd: pwd, email: email})
         .then(() => res.redirect('/'));
 });
 
 
-// app.get('/', (req, res) => {
-//     res.render('home', {user: req.user});
-// });
+app.get('/', (req, res) => {
+    res.render('home', {user: req.user});
+});
 
 app.get('/login', (req, res) => {
     res.render('login');
 });
+
+//
+// //route de base, sur index.pug,
+//         app.get('/', (req, res, email) => {
+//             Users
+//                 .sync() // création de la table
+//                 .then(() => {
+//                     Users
+//                         .findOne({where: {email: email}})
+//                         .then(() => {
+//                             res.render('home', {email}); //récupère et envoie les données
+//                         })
+//                 });
+// });
+//
 
 
 app.post('/login', passport.authenticate('local', {
@@ -107,6 +102,7 @@ app.post('/login', passport.authenticate('local', {
         failureRedirect: '/login',
     })
 );
+
 
 console.log("server on 3k");
 app.listen(3000);
